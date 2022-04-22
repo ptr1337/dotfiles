c0ebe051fb6f1aa845f42c34d4a19b410126b1fc
Peter Jung <admin@ptr1337.dev>
2022-04-23 00:54:35 +0200

    update modprobed-db

--- scripts/misc/reinstall.sh
+++ scripts/misc/reinstall.sh
@@ -2,13 +2,15 @@
 
 wget https://build.cachyos.org/cachyos-repo.tar.xz
 tar xvf cachyos-repo.tar.xz
+chmod +x cachyos-repo.sh
 sudo ./cachyos-repo.sh
 wget https://raw.githubusercontent.com/ptr1337/dotfiles/master/scripts/misc/makepkg.conf
 sudo mv makepkg.conf /etc/makepkg.conf
-wget https://raw.githubusercontent.com/ptr1337/dotfiles/master/scripts/misc/modprobed.db .config/
+wget https://raw.githubusercontent.com/ptr1337/dotfiles/master/config/modprobed.db
+mv modprobed.db
 
 
-paru -S --noconfirm nextcloud-client llvm clang lld llvm-libs compiler-rt mesa ungoogled-chromium cachyos discord-canary flameshot krusader-git cachyos-gaming-meta bitwarden keepassxc latte-dock mold-git 64gram-desktop session-desktop-bin cpu-x-git flameshot bleachbit eddie-ui-git hummingbird-bin makepkg-optimize-mold github-desktop-bin atom bleachbit meld smartgit thunderbird zsh xlayoutdisplay chromium-extension-web-store beautysh autopep8 shellcheck xclip vscodium vscodium-marketplace vscodium-features micro zenmonitor3-git zsh aria2-fast
+paru -S --noconfirm nextcloud-client llvm clang lld llvm-libs compiler-rt mesa ungoogled-chromium cachyos discord-canary flameshot krusader-git cachyos-gaming-meta bitwarden keepassxc latte-dock mold-git 64gram-desktop session-desktop-bin cpu-x-git flameshot bleachbit eddie-ui-git hummingbird-bin makepkg-optimize-mold github-desktop-bin atom bleachbit meld smartgit thunderbird zsh xlayoutdisplay chromium-extension-web-store beautysh autopep8 shellcheck xclip vscodium-bin micro zenmonitor3-git zsh aria2-fast element-desktop
 
 mkdir projects
 mkdir projects/cachyos
@@ -16,11 +18,10 @@ mkdir projects/kernel
 mkdir repo 
 git clone https://github.com/ptr1337/dotfiles.git .dotfiles
 cd repo
-git clone https://github.com/ptr1337/toolchain.git
 git clone https://github.com/ptr1337/arch-packages.git
 git clone https://github.com/Frogging-Family/nvidia-all
 
-xlayoutdisplay -p DP-4 -o DP-4 -o DP-2
+xlayoutdisplay -p DP-2 -o DP-2 -o DP-4
 
 # kwinrc
 #[Compositing]
