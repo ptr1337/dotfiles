af9f8433b609c11da74c378cf936ab7ccb493fb2
Peter Jung <admin@ptr1337.dev>
2022-06-02 07:28:28 +0200

    update reinstall.sh and makepkg.conf

--- scripts/misc/reinstall.sh
+++ scripts/misc/reinstall.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-wget https://build.cachyos.org/cachyos-repo.tar.xz
-tar xvf cachyos-repo.tar.xz
-chmod +x cachyos-repo.sh
-sudo ./cachyos-repo.sh
-wget https://raw.githubusercontent.com/ptr1337/dotfiles/master/scripts/misc/makepkg.conf
-sudo mv makepkg.conf /etc/makepkg.conf
-wget https://raw.githubusercontent.com/ptr1337/dotfiles/master/config/modprobed.db
-mv modprobed.db
-
-
-paru -S --noconfirm nextcloud-client llvm clang lld llvm-libs compiler-rt mesa ungoogled-chromium cachyos discord-canary flameshot krusader-git cachyos-gaming-meta bitwarden keepassxc latte-dock mold-git 64gram-desktop session-desktop-bin cpu-x-git flameshot bleachbit eddie-ui-git hummingbird-bin makepkg-optimize-mold github-desktop-bin atom bleachbit meld smartgit thunderbird zsh xlayoutdisplay chromium-extension-web-store beautysh autopep8 shellcheck xclip vscodium-bin micro zenmonitor3-git zsh aria2-fast element-desktop
+paru -S --noconfirm llvm clang lld llvm-libs compiler-rt mesa ungoogled-chromium cachyos discord-canary flameshot krusader-git cachyos-gaming-meta bitwarden keepassxc latte-dock mold-git 64gram-desktop-bin cpu-x-git flameshot bleachbit eddie-ui-git makepkg-optimize-mold github-desktop-bin atom bleachbit meld smartgit thunderbird zsh xlayoutdisplay chromium-extension-web-store beautysh autopep8 shellcheck xclip vscodium-bin micro zenmonitor3-git zsh aria2-fast element-desktop buildcache-git
 
 mkdir projects
 mkdir projects/cachyos
 mkdir projects/kernel
 mkdir repo
-git clone https://github.com/ptr1337/dotfiles.git .dotfiles
 cd repo
 git clone https://github.com/ptr1337/arch-packages.git
 git clone https://github.com/Frogging-Family/nvidia-all
+git clone git@github.com:CachyOS/linux-cachyos.git
+cd ..
+cd projects/cachyos
+git clone git@github.com:CachyOS/calamares-config.git
+git@github.com:CachyOS/CachyOS-Live-ISO.git
+git@github.com:CachyOS/CachyOS-PKGBUILDS.git
+git@github.com:CachyOS/CachyOS-Browser-Common.git
+git@github.com:CachyOS/CachyOS-Settings.git
+cd ..
+cd kernel
+git clone git@github.com:CachyOS/linux.git
+git clone git@github.com:CachyOS/linux-cachyos.git
+git clone git@github.com:ptr1337/kernel-patches.git
+git clone git@github.com:ptr1337/linux-cacule.git
 
 xlayoutdisplay -p DP-2 -o DP-2 -o DP-4
 
