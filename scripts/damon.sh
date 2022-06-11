4375d4bdb4bab9f6382351fd84a7f59456457df5
ptr1337 <ptr1337@cachyos.org>
2022-06-11 20:46:30 +0000

    Update damon.sh

--- scripts/damon.sh
+++ scripts/damon.sh
@@ -2,11 +2,11 @@
 
 sudo mount -t debugfs none /sys/kernel/debug/
 
-sudo echo 500 > /sys/module/damon_lru_sort/parameters/hot_thres_access_freq
-sudo echo 120000000 > /sys/module/damon_lru_sort/parameters/cold_min_age
-sudo echo 10 > /sys/module/damon_lru_sort/parameters/quota_ms
-sudo echo 1000 > /sys/module/damon_lru_sort/parameters/quota_reset_interval_ms
-sudo echo 500 > /sys/module/damon_lru_sort/parameters/wmarks_high
-sudo echo 400 > /sys/module/damon_lru_sort/parameters/wmarks_mid
-sudo echo 200 > /sys/module/damon_lru_sort/parameters/wmarks_low
-sudo echo Y > /sys/module/damon_lru_sort/parameters/enabled
+echo 500 > sudo /sys/module/damon_lru_sort/parameters/hot_thres_access_freq
+echo 120000000 > sudo /sys/module/damon_lru_sort/parameters/cold_min_age
+echo 10 > sudo /sys/module/damon_lru_sort/parameters/quota_ms
+echo 1000 > sudo /sys/module/damon_lru_sort/parameters/quota_reset_interval_ms
+echo 500 > sudo /sys/module/damon_lru_sort/parameters/wmarks_high
+echo 400 > sudo /sys/module/damon_lru_sort/parameters/wmarks_mid
+echo 200 > sudo /sys/module/damon_lru_sort/parameters/wmarks_low
+echo Y > sudo /sys/module/damon_lru_sort/parameters/enabled
