b9dcc3acaee62b1e74174a123ccd5d16ee3ea170
ptr1337 <ptr1337@cachyos.org>
2022-07-05 00:32:27 +0000

    Update reinstall.sh

--- scripts/misc/reinstall.sh
+++ scripts/misc/reinstall.sh
@@ -4,6 +4,7 @@ AUR=/home/ptr1337/projects/kernel/aur
 
 paru -S llvm clang lld llvm-libs compiler-rt mesa-git lib32-mesa-git cachyos discord-canary flameshot krusader-git cachyos-gaming-meta bitwarden cpu-x-git flameshot bleachbit eddie-ui-git makepkg-optimize-mold atom bleachbit meld smartgit thunderbird zsh xlayoutdisplay beautysh autopep8 shellcheck xclip vscodium-bin micro zenmonitor3-git zsh aria2-fast forkgram kitty atom betterdiscordctl
 
+echo  "Create directorys"
 mkdir projects
 mkdir projects/cachyos
 mkdir projects/kernel
@@ -14,6 +15,7 @@ git clone git@github.com:CachyOS/linux-cachyos.git
 git clone git@github.com:ptr1337/toolchain.git
 cd ..
 cd projects/cachyos
+echo  "Clone projects stuff"
 git clone git@github.com:CachyOS/calamares-config.git
 git clone git@github.com:CachyOS/CachyOS-Live-ISO.git
 git clone git@github.com:CachyOS/CachyOS-PKGBUILDS.git
@@ -23,13 +25,14 @@ git clone https://github.com/ptr1337/llvm-bolt-scripts.git
 git clone git@github.com:ptr1337/makepkg-optimize.git
 cd ..
 cd kernel
+echo  "Clone kernel stuff"
 git clone git@github.com:CachyOS/linux.git
 git clone git@github.com:CachyOS/linux-cachyos.git
 git clone git@github.com:ptr1337/kernel-patches.git
 git clone git@github.com:ptr1337/linux-cacule.git
 git clone git@github.com:sirlucjan/kernel-patches.git lujan
 
-## Clone aur stuff
+echo  "Clone aur stuff"
 mkdir -p ${AUR}/kernel
 cd ${AUR}/kernel
 git clone ssh://aur@aur.archlinux.org/linux-cachyos.git
@@ -48,7 +51,8 @@ cd ${AUR}
 git clone ssh://aur@aur.archlinux.org/lapce-git.git
 git clone ssh://aur@aur.archlinux.org/mold-git.git
 git clone ssh://aur@aur.archlinux.org/makepkg-optimize-mold.git
+git clone ssh://aur@aur.archlinux.org/gcc-git.git
 
-## Apply resolution
+echo  "Apply resolution"
 xlayoutdisplay -p DP-2 -o DP-2 -o DP-4
 
