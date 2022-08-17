4732ac31ef640cf23b2fe22ec23e36772c8b235f
ptr1337 <ptr1337@cachyos.org>
2022-08-17 11:53:01 +0000

    Update reinstall.sh

--- scripts/misc/reinstall.sh
+++ scripts/misc/reinstall.sh
@@ -2,7 +2,7 @@
 
 AUR=/home/ptr1337/projects/kernel/aur
 
-paru -S llvm clang lld llvm-libs compiler-rt mesa-git lib32-mesa-git cachyos discord-canary flameshot krusader-git cachyos-gaming-meta bitwarden cpu-x-git flameshot bleachbit eddie-ui-git makepkg-optimize-mold atom bleachbit meld smartgit thunderbird zsh xlayoutdisplay beautysh autopep8 shellcheck xclip vscodium-bin micro zenmonitor3-git zsh aria2-fast forkgram kitty atom betterdiscordctl
+paru -S llvm clang lld llvm-libs compiler-rt mesa-tkg-git lib32-mesa-tkg-git cachyos discord-canary flameshot krusader-git cachyos-gaming-meta bitwarden cpu-x-git flameshot bleachbit eddie-ui-git makepkg-optimize-mold atom bleachbit meld smartgit thunderbird zsh xlayoutdisplay beautysh autopep8 shellcheck xclip vscodium-bin micro zenmonitor3-git zsh aria2-fast forkgram kitty atom betterdiscordctl
 
 echo  "Create directorys"
 mkdir projects
@@ -10,7 +10,6 @@ mkdir projects/cachyos
 mkdir projects/kernel
 mkdir repo
 cd repo
-git clone https://github.com/Frogging-Family/nvidia-all
 git clone git@github.com:CachyOS/linux-cachyos.git
 git clone git@github.com:ptr1337/toolchain.git
 cd ..
@@ -20,15 +19,16 @@ git clone git@github.com:CachyOS/calamares-config.git
 git clone git@github.com:CachyOS/CachyOS-Live-ISO.git
 git clone git@github.com:CachyOS/CachyOS-PKGBUILDS.git
 git clone git@github.com:CachyOS/CachyOS-Browser-Common.git
+git clone git@github.com:CachyOS/CachyOS-Browser-Settings.git
 git clone git@github.com:CachyOS/CachyOS-Settings.git
-git clone https://github.com/ptr1337/llvm-bolt-scripts.git
+git clone git@github.com:ptr1337/lvm-bolt-scripts.git
 git clone git@github.com:ptr1337/makepkg-optimize.git
 cd ..
 cd kernel
 echo  "Clone kernel stuff"
 git clone git@github.com:CachyOS/linux.git
 git clone git@github.com:CachyOS/linux-cachyos.git
-git clone git@github.com:ptr1337/kernel-patches.git
+git clone git@github.com:CachyOS/kernel-patches.git
 git clone git@github.com:ptr1337/linux-cacule.git
 git clone git@github.com:sirlucjan/kernel-patches.git lujan
 
